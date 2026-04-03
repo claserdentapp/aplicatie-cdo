37f00630afc6898734f5415263f6ae03b681c766
Dăian David <mdoru849@gmail.com>
2026-04-03 14:23:08 +0300

    fix(vercel): resolve next-intl config export

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: any = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   }
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
