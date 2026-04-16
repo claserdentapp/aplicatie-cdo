fafe9deaa350c4c59a2cba1e54d13bb7280d2274
Dăian David <mdoru849@gmail.com>
2026-04-17 00:04:29 +0300

    fix(edge): remove dynamic import to fix __dirname vercel bug

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -5,8 +5,17 @@ export default getRequestConfig(async () => {
   const c = await cookies();
   const locale = c.get('NEXT_LOCALE')?.value || 'ro';
 
+  let messages;
+  if (locale === 'en') {
+    messages = (await import('../../messages/en.json')).default;
+  } else if (locale === 'de') {
+    messages = (await import('../../messages/de.json')).default;
+  } else {
+    messages = (await import('../../messages/ro.json')).default;
+  }
+
   return {
     locale,
-    messages: (await import(`../../messages/${locale}.json`)).default
+    messages
   };
 });
